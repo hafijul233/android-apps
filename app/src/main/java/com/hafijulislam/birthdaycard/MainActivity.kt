16231ba6080b37795c11ebb6df96b2e12c903ad3
Hafijul Islam <hafijul233@gmail.com>
2022-12-19 01:47:49 +0600

    click event triggered

--- app/src/main/java/com/hafijulislam/birthdaycard/MainActivity.kt
+++ app/src/main/java/com/hafijulislam/birthdaycard/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.hafijulislam.birthdaycard
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ShowToast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
