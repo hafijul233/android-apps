37a9b7e7a791dc1f616e51ad5d3c315dccaa534a
Hafijul Islam <hafijul233@gmail.com>
2022-12-20 02:59:04 +0600

    jUnit Testing added

--- app/src/test/java/com/hafijulislam/birthdaycard/ExampleUnitTest.kt
+++ app/src/test/java/com/hafijulislam/birthdaycard/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.hafijulislam.birthdaycard
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,15 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun generates_number() {
+
+        val dice: Dice = Dice()
+
+        val generatedNumber = dice.roll()
+
+        assertTrue("The value of rollResult was not between 1 and 6", generatedNumber in 1..6)
     }
 }
